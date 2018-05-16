d783d9998283568959af2df933ffd5a79abb47d2
EzuBotz <ezek_bajada@hotmail.co.uk>
2018-05-16 16:13:02 +0200

    Add project files.

---
+++ Cartas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cartas.Startup))]
+namespace Cartas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
